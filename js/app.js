9a0cc9f42f77f45929ec8aadbb6d2c0f85692ea0
Marcio Junior <marciofmjr@hotmail.com>
2021-04-03 10:00:31 -0300

    images and videos optimizeds

--- js/app.js
+++ js/app.js
@@ -7,11 +7,13 @@ var simulateClick = function (elem) {
   var canceled = !elem.dispatchEvent(evt);
 };
 
-function loadVideo(element, videoId, autoplay = false) {
+function loadVideo(element, videoId, width, height, autoplay = false, controls = false) {
   var container = element.parentElement;
   var autoplayParam = autoplay ? "1": "0";
   var muteParam = autoplay ? "1": "0";
-  container.innerHTML = '<iframe width="560" height="315" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" src="https://www.youtube.com/embed/'+videoId+'?rel=0&amp;autoplay='+autoplayParam+'&amp;loop=0&amp;showinfo=0&amp;controls=0&amp;mute='+muteParam+'&amp;enablejsapi=1&amp;widgetid=1"></iframe>';
+  var controlsParam = controls ? "1": "0";
+  container.innerHTML = '<iframe width="'+width+'" height="'+height+'" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" src="https://www.youtube.com/embed/'+videoId+'?rel=0&amp;autoplay='+autoplayParam+'&amp;loop=0&amp;showinfo=0&amp;controls='+controlsParam+'&amp;mute='+muteParam+'&amp;enablejsapi=1&amp;widgetid=1"></iframe>';
 }
 
-setTimeout(function () { simulateClick(document.querySelector('#linkMainVideo')); }, 500)
+// Starts main video automatically
+// setTimeout(function () { simulateClick(document.querySelector('#linkMainVideo')); }, 500)
