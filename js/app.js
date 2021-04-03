2702a379a112da49182c8d909e74cbec25d3f48b
Marcio Junior <marciofmjr@hotmail.com>
2021-04-03 09:11:34 -0300

    set main video to loads automatically

--- js/app.js
+++ js/app.js
@@ -13,3 +13,5 @@ function loadVideo(element, videoId, autoplay = false) {
   var muteParam = autoplay ? "1": "0";
   container.innerHTML = '<iframe width="560" height="315" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" src="https://www.youtube.com/embed/'+videoId+'?rel=0&amp;autoplay='+autoplayParam+'&amp;loop=0&amp;showinfo=0&amp;controls=0&amp;mute='+muteParam+'&amp;enablejsapi=1&amp;widgetid=1"></iframe>';
 }
+
+setTimeout(function () { simulateClick(document.querySelector('#linkMainVideo')); }, 500)
