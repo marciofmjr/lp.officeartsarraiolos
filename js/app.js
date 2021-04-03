c66f88f7c2388e59e662a18ebb636034992c771b
Marcio Junior <marciofmjr@hotmail.com>
2021-04-03 09:08:45 -0300

    adjust main video to loads online on click

--- js/app.js
+++ js/app.js
@@ -1,35 +1,15 @@
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('main_video', {
-    height: '360',
-    width: '640',
-    videoId: 'M7lc1UVf-VE',
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
+var simulateClick = function (elem) {
+  var evt = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    view: window
   });
-}
+  var canceled = !elem.dispatchEvent(evt);
+};
 
-function onPlayerReady(event) {
-  event.target.playVideo();
+function loadVideo(element, videoId, autoplay = false) {
+  var container = element.parentElement;
+  var autoplayParam = autoplay ? "1": "0";
+  var muteParam = autoplay ? "1": "0";
+  container.innerHTML = '<iframe width="560" height="315" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" src="https://www.youtube.com/embed/'+videoId+'?rel=0&amp;autoplay='+autoplayParam+'&amp;loop=0&amp;showinfo=0&amp;controls=0&amp;mute='+muteParam+'&amp;enablejsapi=1&amp;widgetid=1"></iframe>';
 }
-
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-
-function stopVideo() {
-  player.stopVideo();
-}
-
